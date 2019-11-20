d2cf306f5ebe3d9b8777b9aab30cb7a7d022930b
Rajendra <saudrajiv10@gmail.com>
2019-11-20 16:18:37 +0545

    calculator with radio button

---
+++ app/src/main/java/com/raziv/topic1/Image_view.java
@@ -0,0 +1,16 @@
+package com.raziv.topic1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Image_view extends AppCompatActivity {
+
+    
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image_view);
+    }
+}
